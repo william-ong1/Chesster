89a3e76b6682e4c6a88b916c0073564b8638b553
Danyuan Wang <danyuanwang@hotmail.com>
2026-02-11 15:46:27 -0800

    PGN cleaner skeleton

---
+++ data/PGN_cleaner.py
@@ -0,0 +1,3 @@
+# takes in a PGN file and cleans it up, removing all comments and other non-essential information
+# the output should be a a list of seperate games, each game being a string of the game's PGN
+

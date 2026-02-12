fd4e5541acfc92c71bcf78d7c02db2346af6ac99
Raghav Ramesh <raghavtr@uw.edu>
2026-02-11 22:39:59 -0800

    Finished Agent

--- ML/agent.py
+++ ML/agent.py
@@ -1,25 +1,35 @@
-# code to take a model from ML\models and wrap it in a class that can be used to play chess. 
-# this class should take a model for initaliztion and take a board state as input and return a move.
-# the class should also be able to take a move as input and update the board state.
-# include a search function in the agent class with alpha beta pruning.
-
-import torch
 import chess
+
 
 class Agent:
     # model_id is the model id in mongoDB to the model file
     def __init__(self, model_id: str):
-        self.model = torch.load(model)
-        self.board = chess.Board()
+        self.model = model_id  # TODO update this to retreive model from database
+
+        if self.model is None:
+            raise ValueError(f"User does not have a model with id {model_id}")
+
+        self.model.eval()
 
     def get_move(self, board: chess.Board) -> chess.Move:
-        #TODO
-        pass
+        """
+        takes in a board state
+        goes through all legal moves
+        evaluates each move using model
+        returns the best move
+        """
+        legal_moves = list(board.legal_moves)
+        best_move = None
+        best_value = -float("inf")
 
-    def make_move(self, move: chess.Move):
-        #TODO
-        pass
+        for move in legal_moves:
+            board.push(move)
+            value = self.model(
+                board
+            ).item()  # TODO update this to transform board states to features for model input
+            board.pop()
+            if value > best_value:
+                best_move = move
+                best_value = value
 
-    def get_board(self) -> chess.Board:
-        #TODO
-        pass
+        return best_move

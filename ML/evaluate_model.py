21b7e8f50bc58816ae1885ddc03fe33cec8c1819
Danyuan Wang <danyuanwang@hotmail.com>
2026-02-12 13:50:16 -0800

    removed R2 and fixed MAE

--- ML/evaluate_model.py
+++ ML/evaluate_model.py
@@ -4,9 +4,7 @@
 
 import torch
 import torch.nn as nn
-import chess
-from torch.utils.data import DataLoader
-import pymongo
+
 
 class EvaluateModel:
     def __init__(self, model: nn.Module, test_x: torch.Tensor, test_y: torch.Tensor):
@@ -35,18 +33,6 @@ class EvaluateModel:
         
         return mse
 
-    def R2(self) -> float:
-        '''
-        get the data from the database
-        convert the data into a dataloader
-        test the model on the dataloader
-        return the R2 of the model
-        '''
-        self.model.eval()
-        with torch.no_grad():
-            outputs = self.model(self.X)
-            r2 = nn.R2Loss()(outputs, self.y)
-        return r2
     
     def MAE(self) -> float:
         '''
@@ -58,15 +44,15 @@ class EvaluateModel:
         self.model.eval()
         with torch.no_grad():
             outputs = self.model(self.X)
-            mae = nn.MAELoss()(outputs, self.y) 
+            mae = nn.L1Loss()(outputs, self.y) 
 
         return mae
 
 
-    def save_results(self, mse: float, r2: float, mae: float):
+    def save_results(self, mse: float, mae: float):
         '''
         save the results to a local file in model_evals
 
         '''
         with open("model_evals/results.txt", "a") as f:
-            f.write(f"MSE: {mse}, R2: {r2}, MAE: {mae}\n")
+            f.write(f"MSE: {mse}, MAE: {mae}\n")

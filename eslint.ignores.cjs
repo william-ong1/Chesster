1288ca7a2a2eda89b3b0133cf293455ccd639113
masont7 <166885064+masont7@users.noreply.github.com>
2026-02-12 13:55:03 -0800

    Implemented workflow for Python and Typescript files (#17)
    * Updated workflow to use proper tools for linting and unit testing
    
    * Added config files:
    - pylintrc: defines linter rules for Python based on the Google Python style guide
    - pyproject.toml: defines the max line length for Black Formatter
    - .prettierr.cjs and .prettierrc.json: config for the prettier auto
      formatter (used by GTS to format TypeScript code)
    - eslint.config.cjs and eslint.ignores.cjs: config for eslint linter
      (used by GTS to lint TypeScript code)
    - package-lock.json (auto-generated) and package.json: used for  project config, allows running the
      following commands:
      - `npm run test`: runs all commands used for CI
      - `npm run lint` and `npm run posttest`: runs the gts linter
      - `npm run clean`: runs `gts clean`
      - `npm run compile`: compiles Typescript code
      - `npm run fix`: runs `gts fix`
      - `npm run prepare` and `npm run pretest`: runs compile command
      - `npm run unit-test`: runs Vitest unit tests only
    - tsconfig.json: TypeScript configuration settings
    - vitest.config.ts: Vitest configuration settings
    
    * Created mock test files to work with CI

---
+++ eslint.ignores.cjs
@@ -0,0 +1,13 @@
+module.exports = [
+  'build/',
+  'dist/',
+  'coverage/',
+  '*.test.js',
+  '*.test.js.map',
+  '*.test.d.ts',
+  '*.test.d.ts.map',
+  '*.d.ts',
+  '*.d.ts.map',
+  '*.js',
+  '*.js.map',
+];

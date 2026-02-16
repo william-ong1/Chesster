4dfe5d411211e8f1a4b4c3e4be91bee7e56bd294
Danyuan Wang <danyuanwang@hotmail.com>
2026-02-16 14:46:21 -0800

    housekeeping changes suggested by mason

--- ML/evaluate_model.py
+++ ML/evaluate_model.py
@@ -1,6 +1,5 @@
 # evaluate the model on player data to see how well it matches the player
-# pull data from mongoDB and use it to test the model.
-# after testing, save the results to mongoDB
+# after testing, save the results to local file
 
 import torch
 import torch.nn as nn
@@ -10,7 +9,8 @@ class EvaluateModel:
     def __init__(self, model: nn.Module, test_x: torch.Tensor, test_y: torch.Tensor):
         '''
         model is the model to test
-        dataloader is the dataloader to test the model on
+        test_x is the features to test the model on
+        test_y is the y to test the model on
         '''
         self.model = model
         self.X = test_x
@@ -25,7 +25,6 @@ class EvaluateModel:
         return the MSE of the model
 
         '''
-        
         self.model.eval()
         with torch.no_grad():
             outputs = self.model(self.X)

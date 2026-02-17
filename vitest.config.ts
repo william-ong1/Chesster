576c98e164a4171de28cd95e2f8e75fbe0d4d60f
Mason Tepper <mason.tep@gmail.com>
2026-02-17 00:19:31 -0800

    Added code coverage testing for TypeScript to the CI workflow

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,23 @@ export default defineConfig({
   test: {
     environment: 'node',
     include: ['tests/**/*.ts', 'tests/**/*.tsx'],
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      include: ['./**/*.{ts,tsx}'],
+      exclude: [
+        // files/patterns to ignore (e.g., test setup files, type declarations)
+        './**/index.tsx',
+        './**/setupTests.ts',
+        './**/*.d.{ts,tsx}',
+      ],
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
+      reporter: ['text', 'html', 'json'],
+    },
   },
 });

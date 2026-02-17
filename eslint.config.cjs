ed27f2a47f8b794fc85c263c4acb18c2a65f2f22
Mason Tepper <mason.tep@gmail.com>
2026-02-17 14:59:42 -0800

    Disabled linter for config files

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 let customConfig = [];
 let hasIgnoresFile = false;
 try {
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
